97c8d393bede469a3a48947f7eb70258e4f301b4
Allison Grekin <allison_grekin@hotmail.com>
2017-02-13 12:50:01 +0000

    completed ex 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  get 'answer', to: 'coaching#answer'
+
+  get 'ask', to: 'coaching#ask'
+
+  root to: 'coaching#ask', as: :welcome
+
+  # get 'coaching/answer', to: 'coaching#answer'
+
+  # get 'coaching/ask', to: 'coaching#ask'
+
+  # root to: 'coaching#answer', as: :welcome
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
